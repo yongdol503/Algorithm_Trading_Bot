245fb3048576301001b6675d5195f2d96bae9942
yongdol503 <yongdol503@gmail.com>
2021-02-25 13:08:00 +0900

    Add account_test.py

---
+++ account_test.py
@@ -0,0 +1,28 @@
+import os
+import jwt
+import json
+import uuid
+import hashlib
+from urllib.parse import urlencode
+
+import requests
+
+UPBIT_OPEN_API_ACCESS_KEY=open("UPBIT_OPEN_API_ACCESS_KEY.cfg","r").read()
+UPBIT_OPEN_API_SECRET_KEY=open("UPBIT_OPEN_API_SECRET_KEY.cfg","r").read()
+UPBIT_OPEN_API_SERVER_URL="https://api.upbit.com"
+
+payload = {
+    'access_key': UPBIT_OPEN_API_ACCESS_KEY,
+    'nonce': str(uuid.uuid4()),
+}
+
+jwt_token = jwt.encode(payload, UPBIT_OPEN_API_SECRET_KEY)
+authorize_token = 'Bearer {}'.format(jwt_token)
+headers = {"Authorization": authorize_token}
+
+res = requests.get(UPBIT_OPEN_API_SERVER_URL + "/v1/accounts", headers=headers)
+
+for currency in res.json():
+    for key in currency.keys():
+        print(key," : ",currency[key])
+    print()
